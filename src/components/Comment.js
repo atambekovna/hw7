c7a6b3486f8b96d5d1d5a23f2778c280d7a488d4
atambekovna <atambekovna1411@gmail.com>
2022-04-21 17:37:03 +0600

    wrapper

---
+++ src/components/Comment.js
@@ -0,0 +1,26 @@
+import CommentDate from "./CommentDate";
+import UserInfo from "./UserInfo";
+import CommentTextWrapper from "../UI/CommentTextWrapper";
+import './Comment.css'
+
+function Comment (props){
+    return(
+        <CommentTextWrapper className='Comment'>
+
+
+        <UserInfo author={props.author}/>
+
+        <div className='Comment-text'>
+
+          {props.text}
+        <CommentDate date={props.date}/>
+
+
+        </div>
+
+
+      </CommentTextWrapper>
+    )
+}
+
+export default Comment
---
+++ src/components/CommentAvatar.js
@@ -0,0 +1,12 @@
+
+function CommentAvatar (props){
+    return <div>
+        
+        <img  
+        src={props.author.avatarUrl} 
+        alt={props.author.name}/>
+
+    </div>
+}
+
+export default CommentAvatar
